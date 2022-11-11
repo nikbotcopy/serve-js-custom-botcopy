d57047b8493ce2230b712638fb67e80889b6535b
Nikhil <nikhil@botcopy.com>
2022-11-11 10:25:46 +0530

    Updare

--- public/botcopy-custom-code.js
+++ public/botcopy-custom-code.js
@@ -1,1 +1,29 @@
-const x = {};
+window.addEventListener("botcopy-events", function (e) {
+  switch (e.detail.type) {
+    case "bc-window-open":
+      // do something
+      alert("Window Open");
+      break;
+
+    case "bc-window-close":
+      alert("Window close");
+
+      // do something
+      break;
+  }
+});
+
+function closeWindow() {
+  Botcopy.closeWindow();
+}
+function openWindow() {
+  Botcopy.openWindow();
+}
+
+function sendEvent(text, contextName, lifespan, parameters = {}) {
+  Botcopy.sendEvent(text, {
+    name: contextName || "context-name",
+    lifespanCount: lifespan || 2,
+    parameters: parameters,
+  });
+}
